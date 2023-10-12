a1487d9febe39eeee575121ceb52303a7e70333b
Roberto Yunior Dominguez Molina <robertodominguez@uagrm.edu.bo>
2023-10-12 15:30:40 -0400

    cambios

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reconocimiento de voz</title>
+    
+</head>
+<body>
+   
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AI Reconocimiento de Audio</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/estilo.css')}}">
+</head>
+<body>
+    <div id="texto"></div>
+   <script src="{{asset('assets/script.js')}}"></script>
+</body>
+</html>
